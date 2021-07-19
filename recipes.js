e5daef8e9c2e959f4d9e2e6f6c63e104dbe0f33e
Roger Zavala <rogeliozavala15@gmail.com>
2021-07-18 18:33:18 -0700

    init recipe object

---
+++ recipes.js
@@ -0,0 +1,7 @@
+const recipes = {
+    ['bahn mi']: {
+        Directions: 'Whisk half-and-half, ricotta cheese, eggs, melted butter, and lemon extract together in a separate bowl until smooth. Pour into the flour mixture and mix until thoroughly combined.',
+        ingredients,
+        image:'./images/bahn-mi.jpg',
+    }
+}
