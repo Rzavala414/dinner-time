e57a23ca9e567f4a7597c1419eb7b5fc44816911
Roger Zavala <rogeliozavala15@gmail.com>
2021-07-24 11:27:51 -0700

    added structure to db

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import recipes from '../recipes.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={recipes['bahn mi'].image} alt="bahn mi" />
+      <ol>
+        <li>{recipes['bahn mi'].ingredients}</li>
+      </ol>
+      <p>{recipes['bahn mi'].directions}</p>
       
     </div>
   );
---
+++ src/recipes.js
@@ -0,0 +1,10 @@
+const recipes = {
+    'bahn mi': {
+        directions: 'Whisk half-and-half, ricotta cheese, eggs, melted butter, and lemon extract together in a separate bowl until smooth. Pour into the flour mixture and mix until thoroughly combined.',
+        ingredients: 'bread',
+        category: 'dinner',
+        image: require('./img/bahn-mi.png').default
+    }
+}
+
+export default recipes
