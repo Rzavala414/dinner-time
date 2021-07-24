be91db7af2b0bae17dc21a924ba91b2ced4a4757
Roger Zavala <rogeliozavala15@gmail.com>
2021-07-24 12:53:39 -0700

    refactered db

--- src/app/App.js
+++ src/app/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import recipes from '../recipes.js'
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <img src={recipes['bahn mi'].image} alt="bahn mi" />
-      <ol>
-        <li>{recipes['bahn mi'].ingredients}</li>
-      </ol>
-      <p>{recipes['bahn mi'].directions}</p>
-      
+      <img src={recipes[0].image} alt="bahn mi" />
+      {recipes[0].nameOfFood}
     </div>
   );
 }
--- src/recipes.js
+++ src/recipes.js
@@ -1,10 +1,28 @@
-const recipes = {
-    'bahn mi': {
-        directions: 'Whisk half-and-half, ricotta cheese, eggs, melted butter, and lemon extract together in a separate bowl until smooth. Pour into the flour mixture and mix until thoroughly combined.',
-        ingredients: 'bread',
-        category: 'dinner',
-        image: require('./img/bahn-mi.png').default
-    }
-}
+const recipes = [
+   { 
+     id: 1,
+     nameOfFood: 'bahn mi', 
+     directions: 'Whisk half-and-half, ricotta cheese, eggs, melted butter, and lemon extract together in a separate bowl until smooth. Pour into the flour mixture and mix until thoroughly combined.',
+     ingredients: 'bread',
+     category: 'dinner',
+     image: require('./img/bahn-mi.png').default
+   },
+   { 
+     id: 2,
+     nameOfFood: 'Carne Asada Tacos', 
+     directions: 'Whisk half-and-half, ricotta cheese, eggs, melted butter, and lemon extract together in a separate bowl until smooth. Pour into the flour mixture and mix until thoroughly combined.',
+     ingredients: 'bread',
+     category: 'dinner',
+     image: require('./img/bahn-mi.png').default
+   },
+   { 
+     id: 3,
+     nameOfFood: 'Birria', 
+     directions: 'Whisk half-and-half, ricotta cheese, eggs, melted butter, and lemon extract together in a separate bowl until smooth. Pour into the flour mixture and mix until thoroughly combined.',
+     ingredients: 'bread',
+     category: 'dinner',
+     image: require('./img/bahn-mi.png').default
+   },
+]
 
 export default recipes
