c7fdf13023a8216ddd9188686b243d38285d35b8
Roger Zavala <rogeliozavala15@gmail.com>
2021-10-29 22:30:39 -0700

    changed img ulr for deployment

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -4,8 +4,12 @@ import './RecipeCard.css';
 export default function RecipeCard(props){
     const { id, dishName, ingredients, img } = props.recipe;
     
-    // direct user to single recipe
-    const imageURL = `/recipe?id=${id}`;
+    // directs user to a single recipe
+    // production URL;
+    // const imageURL = `/recipe?id=${id}`;
+
+    // build URL
+    const imageURL = `https://deploy-preview-13--flamboyant-lewin-942503.netlify.app/recipe?id=${id}`;
     
     return(
         <div className="recipe-card">
