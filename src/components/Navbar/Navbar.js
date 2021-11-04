7afde0d5ecf3b6624274f9b6cb4442e866dd66f3
Roger Zavala <rogeliozavala15@gmail.com>
2021-11-04 13:37:47 -0700

    fixed nav styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faUtensils, faSearch } from '@fortawesome/free-solid-svg-icons'
 import './Navbar.css';
 
 export default function Navbar() {
     return(
-        <nav>
-            <h1 className="logo"> Dinner <span>Time</span> <FontAwesomeIcon className="utensils-icon" icon={faUtensils} /> </h1> 
-            <Link to='/'>Random Recipe</Link>
-            <Link to='/allrecipes'>All Recipes</Link>
-        </nav>
+            <nav>
+                <Link to='/'>Random Recipe</Link>
+                <span className="logo"> Dinner <span>Time</span> <FontAwesomeIcon className="utensils-icon" icon={faUtensils} /> </span> 
+                <Link to='/allrecipes'><FontAwesomeIcon className="search-icon" icon={faSearch} /> </Link>
+            </nav>
     );
 }
